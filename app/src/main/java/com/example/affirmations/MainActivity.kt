27c1d1d73301277397d92819038e4ba1121595f0
Eslam Gad <eslamhendy990@gmail.com>
2021-12-28 23:17:27 -0800

    First Commit

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+
+        val dataSet = Datasource().loadAffirmations()
+
+        recyclerView.adapter = ItemAdapter(this, dataSet)
+
+    }
+}
